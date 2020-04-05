b15c50cceaadf959b19f6994390e4e694d2c0d46
Abhishek Bhonsle <abhishek.bhonsle234@gmail.com>
2020-04-05 12:57:41 +0530

    settings.py updated for postgres

--- RyderBackend/settings.py
+++ RyderBackend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'FareModel',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -74,12 +78,12 @@ WSGI_APPLICATION = 'RyderBackend.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
     }
-}
-
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)    
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
